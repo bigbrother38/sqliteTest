8214be45df418f5b7347c8f8bf4d6e9eb2cf81ac
bigbrother38 <weichun.wu@gmail.com>
2015-12-08 07:47:12 +0800

    commit

--- Scripts/Price.js
+++ Scripts/Price.js
@@ -7,7 +7,9 @@ $( document ).ready(function() {
 	var a = $("#simpleTable").stupidtable();
 	var b = $("#simpleTable2").stupidtable();
 	a.add(b).bind('aftertablesort', function (event, data) {
-		$(this).find('tbody td').css('padding-top','');
+		$(this).find('tbody tr').css('padding-top','');
+		var theadhigh = $("thead", $(this)).height();
+		$($("tr", $(this))[1]).find('td').css("padding-top", theadhigh+"px");
 	});
 	
 	//table scroll
@@ -35,8 +37,13 @@ $( document ).ready(function() {
 
 	//click refresh tr padding-top, thead top
 	$('#ui-id-2').add($('#ui-id-3')).bind('click', function(){
-		high = 	$(this).offset().top;
-	//	$("thead", table).css("top", high+"px");
+		high = $('ul').height()+60;
+		$("thead", table).css("top", high+"px");
+		$("thead", table2).css("top", high+"px");
+		var theadhigh = $("thead", table).height();
+		$($("tr", table)[1]).find('td').css("padding-top", theadhigh+"px");
+		theadhigh = $("thead", table2).height();
+		$($("tr", table2)[1]).find('td').css("padding-top", theadhigh+"px");
 	})
 
 });
